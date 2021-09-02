ead77ec3591db87cbd76ce0a54e54a6d8a0380cd
taylorreichner <taylorreichner@yahoo.com>
2021-09-02 14:07:36 -0700

    removed type module from package lock and used require statements

--- setup-db.js
+++ setup-db.js
@@ -1,4 +1,4 @@
-import pool from './lib/utils/pool.js';
-import setup from './data/setup.js';
+const pool = require('./lib/utils/pool.js');
+const setup = require('./data/setup.js');
 
 setup(pool);
